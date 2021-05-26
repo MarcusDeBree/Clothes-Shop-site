b1aa27070c3fddfa7c32ea4135d7a3ea5f8a1835
MarcusDeBree <jokobs95@mail.ru>
2021-05-26 17:24:11 +0300

    Update settings.py

--- ClothesSite/settings.py
+++ ClothesSite/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY", "Not a secret! Delete this arg in production!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
